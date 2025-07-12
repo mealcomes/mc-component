792fd1a388940be1824810c4ffec64ff878a98a9
mealcomes <2013213920@qq.com>
2025-07-12 19:18:57 +0800

    chore: 实现 BEM 规范

---
+++ packages/utils/create.ts
@@ -0,0 +1,72 @@
+// block element modifier (BEM 规范)
+// m-button  ->  m-button__element  ->  m-button__element--disabled
+// is-checked is-enabled
+
+// m-button-box__element--modifier
+function _bem(
+    prefixName: string,
+    blockSuffix: string,
+    element: string,
+    modifier: string
+) {
+    if (blockSuffix) {
+        prefixName += `-${blockSuffix}`;
+    }
+    if (element) {
+        prefixName += `__${element}`;
+    }
+    if (modifier) {
+        prefixName += `--${modifier}`;
+    }
+    return prefixName;
+}
+
+function createBEM(prefixName: string) {
+    const b = (blockSuffix: string = '') => _bem(prefixName, blockSuffix, '', '')
+    const e = (element: string = '') => element ? _bem(prefixName, '', element, '') : '';
+    const m = (modifier: string = '') => modifier ? _bem(prefixName, '', '', modifier) : '';
+
+    const be = (blockSuffix: string = '', element: string = '') =>
+        blockSuffix && element ? _bem(prefixName, blockSuffix, element, '') : '';
+    const bm = (blockSuffix: string = '', modifier: string = '') =>
+        blockSuffix && modifier ? _bem(prefixName, blockSuffix, '', modifier) : '';
+    const em = (element: string = '', modifier: string = '') =>
+        element && modifier ? _bem(prefixName, '', element, modifier) : '';
+
+    const bem = (blockSuffix: string = '', element: string = '', modifier: string = '') =>
+        blockSuffix && element && modifier
+            ? _bem(prefixName, blockSuffix, element, modifier)
+            : '';
+
+    const is = (name: string, state: boolean) => state ? `is-${name}` : '';
+
+    return {
+        b,
+        e,
+        m,
+        be,
+        bm,
+        em,
+        bem,
+        is
+    }
+}
+
+/**
+ * 创建指定命名空间的 BEM(标签类名)
+ */
+export function createNamespace(name: string) {
+    const prefixName = `m-${name}`;
+    return createBEM(prefixName);
+}
+
+// const bem = createNamespace('icon');
+// console.log(bem.b('box'));
+// console.log(bem.e('element'));
+// console.log(bem.m('modifier'));
+// console.log(bem.be('box', 'element'));
+// console.log(bem.bm('box', 'modifier'));
+// console.log(bem.em('element', 'modifier'));
+// console.log(bem.bem('box', 'element', 'modifier'));
+// console.log(bem.is('checked', true));
+
