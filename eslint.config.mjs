d48dbd3a8945d38eef5cc306d7f84c932b7660e4
mealcomes <2013213920@qq.com>
2025-07-25 22:26:45 +0800

    chore: eslint 配置更改

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -27,12 +27,7 @@ export default defineConfig([
         files: ['**/*.vue'],
         languageOptions: { parserOptions: { parser: tseslint.parser } },
         rules: {
-            'vue/multi-word-component-names': [
-                'error',
-                {
-                    ignores: []
-                }
-            ]
+            'vue/multi-word-component-names': 'off'
         }
     }
 ]);
