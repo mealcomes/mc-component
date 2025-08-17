ebf136fc36851d09b1372df32ba26b0cfe7b0400
mealcomes <2013213920@qq.com>
2025-08-18 02:33:55 +0800

    fix: ts 类型报错

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -7,6 +7,7 @@ import '@vitepress-demo-preview/component/dist/style.css';
 import './style.css';
 import MealComes from 'mealcomes';
 import 'mealcomes/theme-chalk/index.css';
+import { Awaitable } from 'vitepress';
 
 export default {
     extends: DefaultTheme,
@@ -20,7 +21,7 @@ export default {
         app.component('demo-preview', ElementPlusContainer);
         const nprogress = await import('nprogress');
 
-        router.onBeforeRouteChange = nprogress.start;
-        router.onAfterRouteChange = nprogress.done;
+        router.onBeforeRouteChange = nprogress.start as unknown as (to: string) => Awaitable<void | boolean>;
+        router.onAfterRouteChange = nprogress.done as unknown as  (to: string) => Awaitable<void>;
     }
 } satisfies Theme;
